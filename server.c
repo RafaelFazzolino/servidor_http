829f93f338f0a00c31fe4729b11dbb0491b14773
Rafael Fazzolino <fazzolino29@gmail.com>
2016-09-30 23:30:17 -0300

    Comentando..

--- server.c
+++ server.c
@@ -10,18 +10,15 @@ char * get_data(char *texto){
 	char * texto_completo;
 
 	FILE *fp;
-	fp = fopen(texto, "r");
+	fp = fopen(texto, "r");//abrindo arquivo somente para leitura
 	
 	data = malloc(sizeof(char*));
 	texto_completo = malloc(sizeof(char*));
 
 
-	while(fgets(data, 100, (FILE*)fp) != NULL){
-		strcat(texto_completo, data);
-	   	//strcat(texto_completo, "\n");
+	while(fgets(data, 100, (FILE*)fp) != NULL){//Enquanto não chegar ao fim do arquivo
+		strcat(texto_completo, data);//concatenando cada linha do arquivo para gerar o texto completo
 	}
-   	
-   	printf("%s", texto_completo);
 
 	fclose(fp);
 	return texto_completo;
@@ -31,22 +28,27 @@ void trata_cliente(int socket_cliente) {
 	char buffer[200];
 	char texto[200];
 	int tamanho_recebido, tamanho_envio, i;
+	//pega a info, retornando o tamanho dessa info
 	if((tamanho_recebido = recv(socket_cliente, buffer, 200, 0)) < 0)
 		printf("Erro no recv()\n");
-	buffer[tamanho_recebido] = '\0';
-	printf("Servidor!\n Recebi algo: %s \n", buffer);
+	buffer[tamanho_recebido] = '\0';//adicionando final da string
+
+	printf("Recebi algo: %s \n", buffer);
+
+	//É uma requisição GET?
 	if(buffer[0] == 'G' && buffer[1] == 'E' && buffer[2] == 'T'){
-		i=5;
-		while(buffer[i] != ' '){
+		i=5;//o nome do arquivo começa na posição 5 do vetor, acabando no próximo espaço em branco.
+		while(buffer[i] != ' '){ //faço o parser para obter o arquivo desejado pelo cliente
 			texto[i-5] = buffer[i];
 			++i;
 		}
 	}
 
-	strcpy(texto, get_data(texto));
-	printf("RESULTADO: %s\n", texto);
+	strcpy(texto, get_data(texto));//Pegando os dados do arquivo desejado
+
 	while (tamanho_recebido > 0) {
-		tamanho_envio = strlen(texto);
+		tamanho_envio = strlen(texto);//pegando o tamanho do texto
+		//verifica se todos os bytes foram enviados
 		if(send(socket_cliente, texto, tamanho_envio, 0) != tamanho_envio)
 			printf("Erro no envio - send()\n");
 		
@@ -61,7 +63,7 @@ int main(int argc, char *argv[]) {
 	struct sockaddr_in clienteAddr;
 	unsigned short servidorPorta;
 	unsigned int cliente_length;
-	if (argc != 2) { //Caso não tenha entrado com os parâmetros
+	if (argc != 2) { //Caso não tenha entrado com os parâmetros certos
 		printf("Uso: %s <Porta>\n", argv[0]);
 		exit(1);
 	}
